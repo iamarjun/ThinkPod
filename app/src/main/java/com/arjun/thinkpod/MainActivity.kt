d750fc97cc868523f12b69dc362594b1ac096e4d
iamarjun <arjunmanoj1995@gmail.com>
2020-09-23 01:20:25 +0530

    force dark mode

--- app/src/main/java/com/arjun/thinkpod/MainActivity.kt
+++ app/src/main/java/com/arjun/thinkpod/MainActivity.kt
@@ -2,6 +2,7 @@ package com.arjun.thinkpod
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -9,5 +10,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        //Force Dark Mode
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
     }
 }
